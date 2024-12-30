e0cef73331bce17cc02ccc78303601857f310ccc
Prosenjit Mondol <prosen1156@gmail.com>
2024-12-30 12:56:46 +0600

    ok

--- no33.java
+++ no33.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package LabSet09Array;
 
 import java.util.Arrays;
 
-/**
- *
- * @author HP
- */
+
 public class no33 {
     public static void main(String[] args) {
         int nums[] = {20, 20, 30, 40, 50, 50, 50};
